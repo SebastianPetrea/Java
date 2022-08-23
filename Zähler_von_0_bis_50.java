f5e1e55eb46a0e23b9db41c864454f60dc659e03
SebastianPetrea <80840012+SebastianPetrea@users.noreply.github.com>
2022-08-23 12:05:57 +0200

    Counts from 0 to 50
    A program that counts from 0 to 50 and separates the numbers next to each other by commas.

---
+++ Zähler_von_0_bis_50.java
@@ -0,0 +1,16 @@
+/* ####################################################
+Einsendeaufgabe 3.2
+#################################################### */
+public class Zähler{
+
+	public static void main(String[] args) {
+		for (int i = 1; i < 51; i++) {
+			System.out.print(i);
+			if (i < 50)
+				System.out.print(",");
+		}
+
+
+	}
+
+}
