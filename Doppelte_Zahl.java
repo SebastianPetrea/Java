136d5da0dc0f35ce4dea3b16ca294b93a9483cba
SebastianPetrea <80840012+SebastianPetrea@users.noreply.github.com>
2022-08-23 12:08:15 +0200

    Outputs twice the number

---
+++ Doppelte_Zahl.java
@@ -0,0 +1,22 @@
+/* ####################################################
+Einsendeaufgabe 3.3
+#################################################### */
+public class Doppelte_Zahl{
+
+	public static void main(String[] args) {
+		
+			int zahl = 1;
+			int ergebnis;
+			while (zahl < 101) {
+				ergebnis = zahl *2;
+				System.out.println(ergebnis);
+				zahl++;
+			}
+			
+			
+			
+		}
+
+	}
+
+
